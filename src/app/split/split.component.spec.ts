62edf6e1425a7df7574d1dc49a622dd22997eae3
Louis RREINE <louis.reine@eleves.enpc.fr>
2020-12-29 20:15:41 +0100

    added split feature - very basic & undocumented for now

--- src/app/split/split.component.spec.ts
+++ src/app/split/split.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { SplitComponent } from './split.component';
 
 describe('SplitComponent', () => {
--- src/app/split/split.component.ts
+++ src/app/split/split.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import * as RecordRTC from 'RecordRTC';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-split',
   templateUrl: './split.component.html',
   styleUrls: ['./split.component.css']
 })
+
+
 export class SplitComponent implements OnInit {
 
-  constructor() { }
+	private record;
+
+	public recording = false;
+
+	private error;
+
+	public recordingDone = false;
+
+	public url;
+
+	public fileUrl;
+
+  constructor(private domSanitizer: DomSanitizer) { 
+  }
+
+  sanitize(url:string){
+  	return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+
+
+  initiateRecording() {
+  	this.recording = true;
+  	let mediaConstraints = {
+  		video: false,
+  		audio: true
+  	};
+  	navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.sucessCallback.bind(this),this.errorCallback.bind(this)) ;
+  }
+
+  stopRecording() {
+  	this.recording = false;
+  	this.record.stop(this.processRecording.bind(this));
+  	this.recordingDone = true;
+  }
+
+  sucessCallback(stream) {
+  	var options = {
+  		mimeType: "audio/wav",
+  		numberOfAudioChannels:1
+  	};
+
+  	var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+  	this.record = new StereoAudioRecorder(stream, options);
+  	this.record.record();
+  }
+
+  errorCallback(error){
+  	this.error = 'Can not play audio in your browser';
+  }
+
+  processRecording(blob) {
+  	this.url = URL.createObjectURL(blob);
+  	this.fileUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
+  }
+
 
   ngOnInit(): void {
   }
+
+
 
 }
